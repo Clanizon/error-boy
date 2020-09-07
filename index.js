547d5cfd6fc5063b28a261403a598b173e8aabb7
Clanizon <devops@clanizon.com>
2020-09-07 16:59:45 +0530

    Working library

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
+const fetch = require("node-fetch");
+
 class ErrorBoy {
   constructor(webHookURL) {
     this.webHookURL = webHookURL;
   }
+
+  init(url) {
+    this.webHookURL = url;
+  }
+
+  setWebHookURL = (url) => {
+    this.webHookURL = url;
+  };
+
+  log = (msg) => {
+    const data = JSON.stringify({
+      text: msg,
+    });
+    console.warn(this.webHookURL);
+    fetch(this.webHookURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      body: data,
+    })
+      .then((response) => {
+        console.log("---ErrorBoy.Sent---");
+      })
+      .catch((error) => {
+        console.error("---ErrorBoy.Failed---", error);
+      });
+  };
 }
 
-exports.log = (msg) => {
-  const data = JSON.stringify({
-    text: msg,
-  });
-
-  fetch(this.webHookURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=UTF-8",
-    },
-    body: data,
-  })
-    .then((response) => {
-      console.log("---ErrorBoy.Sent---");
-    })
-    .catch((error) => {
-      console.error("---ErrorBoy.Failed---");
-    });
-};
+module.exports = new ErrorBoy();
